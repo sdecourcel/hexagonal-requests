10e2ebb219c0a795d9eaf25dce2a59dfa54c3930
Sibylle de Courcel <sibylle44@gmail.com>
2019-05-10 01:15:15 +0200

    update job

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,13 +2,25 @@ class Request < ApplicationRecord
 
   enum status: { unconfirmed: 0, confirmed: 1, accepted: 2, expired: 3}
 
+  validates :email, presence: true
+
+
   # scope :unconfirmed, -> { where(status: "unconfirmed") }
   # scope :confirmed, -> { where(status: "confirmed") }
   # scope :accepted, -> { where(status: "accepted") }
   # scope :expired, -> { where(status: "expired") }
 
   def accept!
-    update(status: "accepted")
+    accepted!
+    # update(status: "accepted")
+  end
+
+  private
+
+
+
+  def asked_cannot_be_greater_than_price
+    errors.add(:price, "Le montant total ne peut être inférieur au total déjà demandé au destinataire.") if (price_cents && asked_cents && (asked_cents > price_cents))
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.asset_host = nil
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
