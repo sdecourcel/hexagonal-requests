14bb0eb3d5f6df1528e3c63129be486b9fec635f
Sibylle de Courcel <sibylle44@gmail.com>
2019-05-09 21:24:42 +0200

    create request form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
 
   def new
-    
+    @request = Request.new
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'new/create'
-  get 'new/update_status_confirmed'
-  get 'new/email_to_confirm'
-  get 'new/email_confirmed'
-  root to: 'pages#home'
+
+  resources :requests, only: [:new, :create] do
+    member do
+      get 'update_status_confirmed', to: 'requests#update_status_confirmed'
+      get 'email_to_confirm', to: 'requests#email_to_confirm'
+      get 'email_confirmed', to: 'requests#email_confirmed'
+    end
+  end
+
+  root to: 'requests#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
